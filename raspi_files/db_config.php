d6c4d6161d866fc492af4681e424801b4612aa6d
root <root@kyle-HP.localdomain>
2022-11-01 16:13:21 -0400

    added necessary .php files to be put on RasPi for frontend communication

---
+++ raspi_files/db_config.php
@@ -0,0 +1,7 @@
+
+<?php   // db_config.php 
+    define('DB_USER', "user"); // db user
+    define('DB_PASSWORD', "password"); // db password (mention your db password here)
+    define('DB_DATABASE', "db_name"); // database name
+    define('DB_SERVER', "localhost"); // db server
+?>
