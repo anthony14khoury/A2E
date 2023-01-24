c4e8e39324e5663d8cde1d855d93ebc86ec05a36
anthonykhoury <anthonykhoury@Ubuntu.anthonykhoury.virtualbox.org>
2023-01-24 13:00:12 -0500

    Hand Tracking Improvements

--- HandTracking/hand_tracking.py
+++ HandTracking/hand_tracking.py
@@ -15,18 +15,57 @@ print(os.path.abspath(os.path.join(src_dir, arch_dir)))
 import Leap
 
 
+# Global Variables
+count = 0
+frame_store = []
+sequence = 0
+sequence_store = []
+
+def create_folders():
+     DATA_PATH = os.path.join('DataCollection')
+     actions = actions = np.array(['a', 'nothing'])
+     
+     for action in actions: 
+          try: 
+               os.makedirs(os.path.join(DATA_PATH, action))
+          except:
+               pass
+
+
 class SampleListener(Leap.Listener):
      def on_connect(self, controller):
           print ("Connected")
 
      def on_frame(self, controller):
-          global maps_initialized
-          global coordinate_map
-          global coordniate_coefficients
 
+          global count
+          global frame_store
+          global sequence
+          global sequence_store
           frame = controller.frame()
-          # if frame.is_valid:
-          print("Frame ID: ", frame.id)
+          
+          if frame.is_valid:
+               if count < 30:                    
+                    # Variables to grab
+                    
+                    count += 1
+                    
+               else:
+                    print("Done with Gathering Data for sequence", sequence)
+                    sequence += 1
+                    if sequence < 10:
+                         sequence_store.append(frame_store)
+                         frame_store = []
+                         count = 0
+                         print("Wait 3 seconds")
+                         sleep(3)
+                         print("Start again")
+                    else:
+                         print("Done with all sequences")
+                         #print data to files
+                         
+                         quit()
+               
 
      def on_exit(self, controller):
           print ("Exited")
@@ -55,4 +94,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+     create_folders()
+     main()
