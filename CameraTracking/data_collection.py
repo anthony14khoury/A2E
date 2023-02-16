a829739a8d5b81a15570ff8a040546ca170a6825
Samir Ziad <samirziad@Samirs-MacBook-Pro.local>
2023-02-16 13:25:58 -0500

    Added message to opencv and added socket connection

--- CameraTracking/data_collection.py
+++ CameraTracking/data_collection.py
@@ -52,75 +52,74 @@ def draw_styled_landmarks(image, results):
 
 
 def collect_data(params, letter):
-     
      # Set mediapipe model
      with mp_holistic.Holistic(model_complexity = 0, min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-          
+
           cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
           print("Camera is connected and Everything is Configured!")
-          
-          
+
+
           while cap.isOpened():
-               
+
                for i in range(5):
                     print("Data Collection is starting in {} seconds!".format(5-i))
                     time.sleep(1.0)
                     ret, frame = cap.read()
-               
+
                SEQUENCE_STORE = []
                for sequence in range(params.SEQUENCE_COUNT):
-                    
+
                     FRAME_STORE = []
                     for frame_num in range(params.FRAME_COUNT):
-                         
+
                          # print("Frame: {}".format(frame_num))
-                         
+
                          # Read Feed
                          ret, frame = cap.read()
-                         
+
                          # Made detections
                          image, results = mediapipe_detection(frame, holistic)
                          draw_styled_landmarks(image, results)
                          keypoints = extract_keypoints(results)
-                         FRAME_STORE.append(keypoints)           
-                         
+                         FRAME_STORE.append(keypoints)
+
                          # Show to screen
                          cv2.imshow('OpenCV Feed', image)
-                                        
+
                          # Show to screen
                          # cv2.imshow('OpenCV Feed', frame)
-                         
+
                          # time.sleep(0.033)
-                         
+
                          # Breaking gracefully
                          if cv2.waitKey(5) & 0xFF == ord('q'):
                               cap.release()
                               cv2.destroyAllWindows()
                               quit()
-                    
+
                     print("Done with Sequence: {}".format(sequence))
                     SEQUENCE_STORE.append(FRAME_STORE)
-                    
+
                     print("Wait 2 seconds \n")
                     time.sleep(2.0)
-                    
-               
+
+
                print("Done with all Sequences for {}".format(letter))
-               
-               
+
+
                # Writing npy files
                target_folder = os.path.join(os.path.join('DataCollection'), letter)
                for i in range(params.SEQUENCE_COUNT):
                     set_of_frames = np.array(SEQUENCE_STORE[i])
                     np.save(target_folder + "/" + letter + str(i), set_of_frames)
-                    
-               
-               
+
+
+
                print("\n Program is Finished \n")
                cap.release()
                cv2.destroyAllWindows()
                quit()
-     
+
      
      
 if __name__ == "__main__":
--- CameraTracking/live_prediction.py
+++ CameraTracking/live_prediction.py
@@ -1,17 +1,28 @@
 from keras.models import load_model
 import cv2
 import numpy as np
-import os
-from matplotlib import pyplot as plt
 import time
 import mediapipe as mp
 from parameters import Params
+import socket
 
 mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-def generic_mediapipe_detection(image, model):
+# constants for prediction script
+getReady = "Prepare to Sign!"
+go = "Go!"
+font = cv2.FONT_HERSHEY_SIMPLEX
+color = (255,0,255)
+fontScale = 2
+thickness = 4
+
+# socket settings
+HOST = "127.0.0.1" # The server's hostname or IP address
+PORT = 65432 # The port used by the server
+
+def generic_mediapipe_detectiqon(image, model):
      image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # Color conversion from BGR -> RGB
      image.flags.writeable = False                  # Image is no longer writable
      image.flags.writeable = True                   # Image is now writable
@@ -52,52 +63,70 @@ def draw_styled_landmarks(image, results):
                               ) 
 
 def prediction(params, model, letters):
-                     
-     # Set mediapipe model
-     with mp_holistic.Holistic(model_complexity = 0, min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-          
-          cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-          print("\nCamera is connected and Everything is Configured!")
-          print("Beginning Predictions:\n")
-          
-          while cap.isOpened():
-               
-               FRAME_STORE = []
-               for frame_num in range(params.FRAME_COUNT):
-                    
-                    # print("Frame: {}".format(frame_num))
-                    
-                    # Read Feed
-                    ret, frame = cap.read()
-                    
-                    # Made detections
-                    image, results = mediapipe_detection(frame, holistic)
-                    draw_styled_landmarks(image, results)
-                    keypoints = extract_keypoints(results)
-                    FRAME_STORE.append(keypoints)
-                    
-                    # Show to screen
-                    cv2.imshow('OpenCV Feed', image)
-                    
-                    # Breaking gracefully
-                    if cv2.waitKey(5) & 0xFF == ord('q'):
-                         cap.release()
-                         cv2.destroyAllWindows()
-                         quit()
-                    
+     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+         # connect to socket
+         s.connect((HOST, PORT))
+         # Set mediapipe model
+         with mp_holistic.Holistic(model_complexity = 0, min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
 
-               prediction = model.predict(np.expand_dims(FRAME_STORE, axis=0))
-               char_index = np.argmax(prediction)
-               confidence = round(prediction[0,char_index]*100, 1)
-               predicted_char = letters[char_index]
-               # print(prediction)
-               print(predicted_char, confidence)
-          
-               # print("New Collection:")
-               print("Wait 2 seconds \n")
-               time.sleep(2.0)
-          
+            #  cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+              cap = cv2.VideoCapture(0)
+              print("\nCamera is connected and Everything is Configured!")
+              print("Beginning Predictions:\n")
 
+              while cap.isOpened():
+
+                   FRAME_STORE = []
+                   for frame_num in range(params.FRAME_COUNT):
+                        # Read Feed
+                        ret, frame = cap.read()
+
+                        # Made detections
+                        image, results = mediapipe_detection(frame, holistic)
+                        draw_styled_landmarks(image, results)
+                        keypoints = extract_keypoints(results)
+                        FRAME_STORE.append(keypoints)
+
+                        # Show to screen with go message
+                        if frame_num < 10:
+                            image = cv2.putText(image, go, (int(len(image[0])/2)-10, int(len(image)/2)), font, fontScale, color, thickness, cv2.LINE_AA)
+                            cv2.imshow('OpenCV Feed', image)
+                            # Breaking gracefully
+                            if cv2.waitKey(5) & 0xFF == ord('q'):
+                                cap.release()
+                                cv2.destroyAllWindows()
+                                quit()
+
+                        # Just Show to screen
+                        cv2.imshow('OpenCV Feed', image)
+
+                        # Breaking gracefully
+                        if cv2.waitKey(5) & 0xFF == ord('q'):
+                             cap.release()
+                             cv2.destroyAllWindows()
+                             quit()
+
+
+                   prediction = model.predict(np.expand_dims(FRAME_STORE, axis=0))
+                   char_index = np.argmax(prediction)
+                   confidence = round(prediction[0,char_index]*100, 1)
+                   predicted_char = letters[char_index]
+                   s.send(predicted_char)
+
+                   # print prediction
+                   print(predicted_char, confidence)
+
+                   print("Wait 2 seconds \n")
+
+                   image = cv2.putText(image, getReady, (int(len(image[0])/2)-200, int(len(image)/2)), font, fontScale, color, thickness, cv2.LINE_AA)
+                   cv2.imshow('OpenCV Feed', image)
+                   # Breaking gracefully
+                   if cv2.waitKey(5) & 0xFF == ord('q'):
+                       cap.release()
+                       cv2.destroyAllWindows()
+                       quit()
+
+                   time.sleep(2.0)
 
 def main():
      
