db9165f12c1140c71df39675cc1000d9b5434cf0
anthony14khoury <anthony14khoury@gmail.com>
2023-02-16 10:17:51 -0500

    to update main

--- HandTracking/Clean.py
+++ HandTracking/Clean.py
@@ -1,7 +1,7 @@
 import os, sys, inspect
 import numpy
 
-# Configurations to Install Leap
+# Configurations to Install Leap 
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 arch_dir = './x64'
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
