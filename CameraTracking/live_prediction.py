38b0bdbb8916b62184c199f67a18802ede43543a
anthony14khoury <anthony14khoury@gmail.com>
2023-03-23 14:08:15 -0400

    Updated models

--- CameraTracking/live_prediction.py
+++ CameraTracking/live_prediction.py
@@ -1,9 +1,9 @@
+from parameters import Params, extract_hand_keypoints
 from keras.models import load_model
-import cv2
+import mediapipe as mp
 import numpy as np
 import time
-import mediapipe as mp
-from parameters import Params, mediapipe_detection, extract_hand_keypoints
+import cv2
 import socket
 # from word_detection import add_spaces
 
@@ -11,7 +11,10 @@ import socket
 params = Params()
 
 # ML Model
-model = load_model('./Models/128_26_15_model_tanh.h5')
+try:
+    model = load_model("./Models/128_26_15_model_tanh.h5")
+except:
+    model = load_model("./A2E/CameraTracking/Models/128_26_15_model_tanh.h5")
 letters = params.LETTERS
 
 # Mediapipe Modules
@@ -23,7 +26,7 @@ mp_hands = mp.solutions.hands
 getReady = "Prepare to Sign!"
 go = "Go!"
 font = cv2.FONT_HERSHEY_SIMPLEX
-color = (255,0,255)
+color = (255,255,255)
 fontScale = 1
 thickness = 2
 
@@ -47,18 +50,21 @@ def draw_styled_landmarks(image, results):
 
 
 # Use CV2 Functionality to create a Video Stream
-# cap = cv2.VideoCapture(0, cv2.CAP_ANY)
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_ANY)
+# cap = cv2.VideoCapture(0)
 while not cap.isOpened():
     pass
 print("Camera is connected")
 
+
+cv2.namedWindow("WindowOutput")
 with mp_hands.Hands(model_complexity=0, min_detection_confidence=0.7, min_tracking_confidence=0.7, max_num_hands=2) as hands:
 
     # Stay open while the camera is activated
     while cap.isOpened():
         
         FRAME_STORE = []
+        hands_count = 0
         
         # Loop through all of the frames
         for frame_num in range(params.FRAME_COUNT):
@@ -71,18 +77,22 @@ with mp_hands.Hands(model_complexity=0, min_detection_confidence=0.7, min_tracki
                 print("Ignoring Empty Camera Frame")
                 continue
             
-            # Made detections
+            # Make detections
             image.flags.writeable = False
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             results = hands.process(image)
             
+            if results.multi_handedness != None:
+                hands_count += len(results.multi_handedness)
+
+            # Draw the Detections on the hand (comment out for PI predictions)
             image.flags.writeable = True
-            # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # Don't need this if you are drawing landmarks
             image = draw_styled_landmarks(image, results)
             FRAME_STORE.append(extract_hand_keypoints(results))
             
             # Display Image
-            cv2.imshow('OpenCV Feed', image)
+            cv2.imshow('WindowOutput', image)
 
             # Breaking gracefully
             if cv2.waitKey(5) & 0xFF == ord('q'):
@@ -90,14 +100,39 @@ with mp_hands.Hands(model_complexity=0, min_detection_confidence=0.7, min_tracki
                 cv2.destroyAllWindows()
                 quit()
 
-
-        prediction = model.predict(np.expand_dims(FRAME_STORE, axis=0))
-        char_index = np.argmax(prediction)
-        confidence = round(prediction[0,char_index]*100, 1)
-        predicted_char = letters[char_index]
-        print(predicted_char, confidence)
+        if hands_count > 0:
+            prediction = model.predict(np.expand_dims(FRAME_STORE, axis=0))
+            char_index = np.argmax(prediction)
+            confidence = round(prediction[0,char_index]*100, 1)
+            predicted_char = letters[char_index]
+            print(predicted_char, confidence)
+            
+        else:
+            print("Nothing")
         
-        # if len(predicted_char) > 1:
+        
+        """ Continuous Camera Share """
+        timeout = time.time() + 1
+        while time.time() < timeout:
+
+            # Read Feed
+            success, image2 = cap.read()
+            
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+            # image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # Don't need this if you are drawing landmarks
+            
+            cv2.imshow('WindowOutput', image2)
+                
+            # Show to screen
+            # image = cv2.putText(image, getReady, (int(len(image[0])/2)-200, int(len(image)/2)), font, fontScale, color, thickness, cv2.LINE_AA)
+            # cv2.imshow('OpenCV Feed', image)
+    
+
+
+# if len(predicted_char) > 1:
         #     #ipdb.set_trace()
         #     curr_sen = np.concatenate((curr_sen,temp))
         #     curr_letters = ""
@@ -108,25 +143,3 @@ with mp_hands.Hands(model_complexity=0, min_detection_confidence=0.7, min_tracki
         #     answer,temp = add_spaces(curr_letters, curr_sen)  #Print out most likely placement of spaces, add dashes if none found
         
         # print(answer)
-        
-        
-        """ Continuous Camera Share """
-        timeout = time.time() + 2
-        while True:
-                    
-            if time.time() > timeout:
-                break
-
-            # Read Feed
-            ret, frame = cap.read()
-                
-            # Show to screen
-            to_screen = "{}: {}%".format(predicted_char, confidence)
-            image = cv2.putText(image, to_screen, (int(len(image[0])/2)-200, int(len(image)/2)), font, fontScale, color, thickness, cv2.LINE_AA)
-            cv2.imshow('OpenCV Feed', image)
-                
-            # Breaking gracefully
-            if cv2.waitKey(5) & 0xFF == ord('q'):
-                cap.release()
-                cv2.destroyAllWindows()
-                quit()
